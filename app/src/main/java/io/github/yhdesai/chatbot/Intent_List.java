026610607366352141c5348a1d2ff8484fa964de
Yash <yhdelectric@gmail.com>
2017-07-09 21:59:23 +0530

    initial commit

--- app/src/main/java/io/github/yhdesai/chatbot/Intent_List.java
+++ app/src/main/java/io/github/yhdesai/chatbot/Intent_List.java
@@ -2,10 +2,7 @@ package io.github.yhdesai.chatbot;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class Intent_List extends AppCompatActivity {
